4a9a6bc6b060f20453dd148a39d5277465ad871d
violettaval <violetvalencia37@gmail.com>
2020-11-16 18:38:15 -0600

    added service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,16 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-const iconSizes = ["192", "512"];
-const iconFiles = iconSizes.map(
-  (size) => `/assets/images/icons/icon-${size}x${size}.png`
-);
-
 const staticFilesToPreCache = [
     "/",
     "/index.js",
     "/db.js",
+    "/index.html",
+    "/style.css",
     "/manifest.webmanifest",
-].concat(iconFiles);
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+];
 
 self.addEventListener("install", function(evt) {
     evt.waitUntil(
